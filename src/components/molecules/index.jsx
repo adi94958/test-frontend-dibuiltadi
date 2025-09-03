be5fc2d2c54252080ade7cffc3ca0502d4e71fe9
adi94958 <117360640+adi94958@users.noreply.github.com>
2025-09-03 23:44:17 +0700

    feat: add Modal component export to molecules index for improved accessibility

--- src/components/molecules/index.jsx
+++ src/components/molecules/index.jsx
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as TextInput } from "./TextInput";
 export { default as Avatar } from "./Avatar";
+export { default as Modal } from "./Modal";
